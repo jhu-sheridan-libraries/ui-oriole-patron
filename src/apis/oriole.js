fa9be2570941523251c2b4fb8669e3ab0b445c78
Dazhi Jiao <djiao@jhu.edu>
2018-11-20 09:21:53 -0500

    Handles errors

--- src/apis/oriole.js
+++ src/apis/oriole.js
@@ -1,4 +1,5 @@
 export const searchOriole = (searchParams) => {
+  console.log('search oriole')
   let url = `${ process.env.REACT_APP_API_ROOT }/oriole-resources?limit=30&query=(title="*${ searchParams.query }*" or description="*${ searchParams.query }*") sortby title`
   return new Promise((resolve, reject) => {
     if (searchParams.query) {
@@ -8,7 +9,12 @@ export const searchOriole = (searchParams) => {
           'Content-Type': 'application/json'
         }
       })
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText)
+        }
+        return response.json()
+      })
       .then(json => resolve(json))
       .catch(error => reject(error))
     } else {
