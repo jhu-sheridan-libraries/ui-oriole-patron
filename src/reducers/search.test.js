2a195341715657db0c6c9cfb7d9742cd95f6be31
Dazhi Jiao <djiao@jhu.edu>
2018-12-07 07:32:56 -0500

    Added test for ORIOLE_FETCH_SUCCESS when it's fetching more records

--- src/reducers/search.test.js
+++ src/reducers/search.test.js
@@ -30,12 +30,23 @@ describe('reducers', () => {
 
   it('should handle ORIOLE_FETCH_SUCCESS', () => {
     const state = { ...initialState, query: query }
-    const data = { resources: [ faker.lorem.word, faker.lorem.word ], totalRecords: 2 }
+    const data = { resources: [ faker.lorem.word(), faker.lorem.word() ], totalRecords: 2 }
     const action = {
       type: actionTypes.ORIOLE_FETCH_SUCCESS,
       payload: { response: data, searchParams: { query: query, isNewSearch: true } }
     }
     expect(reducer(state, action)).toEqual({ ...state, data, meta: { ...state.meta, isFetching: false }})
+  })
+
+  it('should append the resources if it is not a new search', () => {
+    const resources = [ faker.lorem.word(), faker.lorem.word(), faker.lorem.word() ]
+    const state = { ...initialState, data: { resources: [ resources[0] ], totalRecords: 3 }, query: query }
+    const data = { resources: resources.slice(1,3), totalRecords: 3 }
+    const action = {
+      type: actionTypes.ORIOLE_FETCH_SUCCESS,
+      payload: { response: data, searchParams: { query: query, isNewSearch: false } }
+    }
+    expect(reducer(state, action)).toEqual({ ...state, data: { resources, totalRecords: 3 }, meta: { ...state.meta, isFetching: false }})
   })
 
   it('should handle ORIOLE_FETCH_ERROR', () => { 
