40ceb367265de89b39c6353d0c3c7fbdd5dfe324
Dazhi Jiao <djiao@jhu.edu>
2018-12-07 11:44:46 -0500

    Added tests for selectors

--- src/selectors/search.js
+++ src/selectors/search.js
@@ -1,5 +1,5 @@
 export const getPage = state => state.search.meta.page
 export const getQuery = state => state.search.query
 export const getTotalRecords = state => state.search.data.totalRecords
-export const isFetching = state => state.search.isFetching
+export const isFetching = state => state.search.meta.isFetching
 export const getResources = state => state.search.data.resources
