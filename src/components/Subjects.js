9664cbed1c0c7f96d99763b208c107cf206ab9da
mcyzyk <mcyzyk@jhu.edu>
2019-01-28 09:16:37 -0500

    Filter Picker:  Initial commit
    Filter Picker:  Initial commit

--- src/components/Subjects.js
+++ src/components/Subjects.js
@@ -1,24 +1,75 @@
 import React, { Component } from 'react'
 import { Container, Nav, NavLink, NavItem, ListGroup, ListGroupItem } from 'reactstrap'
+import Picker from 'react-mobile-picker'
 
 class Subjects extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      valueGroups: {
+        filterName: 'African Americans'
+      },
+      optionGroups: {
+        filterName: ['Available Filters:','Aeronautics',
+                    'Africa',
+                    'African Americans',
+                    'African diaspora',
+                    'American literature',
+                    'Anthropology',
+                    'Archaeology',
+                    'Area Studies',
+                    'Art and history',
+                    'Asia',
+                    'Astronomy',
+                    'Astrophysics',
+                    'Bioethics',
+                    'Biography',
+                    'Biotechnology',
+                    'Book industries',
+                    'Book reviews',
+                    'Business',
+                    'Career development',
+                    'Chemical engineering',
+                    'Chemistry',
+                    'Classical literature',
+                    'Cognitive science',
+                    'Commerce',
+                    'Computer science',
+                    'Computer security',
+                    'Counseling',
+                    'Dictionaries',
+                    'Earth sciences',
+                    'Economics',
+                    'Education',
+                    'Encyclopedias',
+                    'Energy policy',
+                    'Engineering',
+                    'English language',
+                    'English literature'],
+      }
+    };
   }
+
+  // Update the value in response to user picking event
+  handleChange = (name, value) => {
+    this.setState(({valueGroups}) => ({
+      valueGroups: {
+        ...valueGroups,
+        [name]: value
+      }
+    }));
+  };
+
   render() {
+    const {optionGroups, valueGroups} = this.state;
+
     return (
-      <div>
-        <Container className="subjects-container">
-          <ListGroup>
-            <ListGroupItem>Subject 1</ListGroupItem>
-            <ListGroupItem>Subject 2</ListGroupItem>
-            <ListGroupItem>Subject 3</ListGroupItem>
-            <ListGroupItem>Subject 4</ListGroupItem>
-            <ListGroupItem>Subject 5</ListGroupItem>
-          </ListGroup>
-        </Container>
-      </div>
-    )
+      <Picker
+        height={300}
+        optionGroups={optionGroups}
+        valueGroups={valueGroups}
+        onChange={this.handleChange} />
+    );
   }
 }
 
