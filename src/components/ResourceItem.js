6e85583adef1abe3b6c5d962353195e27c9fe6cc
mcyzyk <mcyzyk@jhu.edu>
2019-03-22 14:49:52 -0400

    Update ResourceItem.js
    Proximidified URL string for a Resource Item

--- src/components/ResourceItem.js
+++ src/components/ResourceItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ResourceItem = ({ record, index }) => (
   <div className='item'>
-      <span className='itemTitle'><a href={ record.url } target='_new'>{ record.title }</a></span><br />
+      <span className='itemTitle'><a href={ "http://proxy.library.jhu.edu/login?url=" + record.url } target='_new'>{ record.title }</a></span><br />
       <span className='itemDescription'>{ record.description }</span>
       <p></p>
   </div>
