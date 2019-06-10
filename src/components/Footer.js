421558628c9993d18ec1a9add1c1d4fc95a2a79c
mcyzyk <mcyzyk@jhu.edu>
2019-06-10 16:20:57 -0400

    Google Tag Manager, final Design changes
    Google Tag Manager, final Design changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Container, Nav, NavLink, NavItem, Navbar } from 'reactstrap'
 
 const Footer = () => {
     return (
-      <Container className="jhu-footer col-12">
+      <Container className="jhu-footer col-8">
         <Nav>
           <Navbar>
             <NavItem><NavLink className="jhu-footer-link" href="https://catalyst.library.jhu.edu/info/libraries"><img id="footer-logo" src="/logo.png" alt="" /></NavLink></NavItem>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,27 +5,31 @@ const Header = () => {
 
     return (
     <div>
-      <Container className="jhu-first-nav col-12">
-          <Navbar>
-            <Nav>
-              <NavItem><NavLink className="jhu-first-nav" href="https://library.jhu.edu" target="_blank">Sheridan Libraries</NavLink></NavItem>
-              <NavItem><NavLink className="jhu-first-nav" href="http://welch.jhmi.edu/welchone/" target="_blank">Welch Medical Library</NavLink></NavItem>
-              <NavItem><NavLink className="jhu-first-nav" href="http://www.sais-jhu.edu/atoz/mason-library-washington-dc" target="_blank">SAIS Library</NavLink></NavItem>
-              <NavItem><NavLink className="jhu-first-nav" href="http://musiclibrary.peabody.jhu.edu/home" target="_blank">Friedheim Music Library</NavLink></NavItem>
-              <NavItem><NavLink className="jhu-first-nav" href="https://aplweb/departments/itsd/iks/Pages/IKS.aspx" target="_blank">APL Library</NavLink></NavItem>
-            </Nav>
-          </Navbar>
+      <Container className="jhu-first-nav col-lg-12 col-md-12">
+        <Row>
+          <Col>
+            <Navbar>
+              <Nav>
+                <NavItem><NavLink className="jhu-first-nav" href="https://library.jhu.edu" target="_blank">Sheridan Libraries</NavLink></NavItem>
+                <NavItem><NavLink className="jhu-first-nav" href="http://welch.jhmi.edu/welchone/" target="_blank">Welch Medical Library</NavLink></NavItem>
+                <NavItem><NavLink className="jhu-first-nav" href="http://www.sais-jhu.edu/atoz/mason-library-washington-dc" target="_blank">SAIS Library</NavLink></NavItem>
+                <NavItem><NavLink className="jhu-first-nav" href="http://musiclibrary.peabody.jhu.edu/home" target="_blank">Friedheim Music Library</NavLink></NavItem>
+                <NavItem><NavLink className="jhu-first-nav" href="https://aplweb/departments/itsd/iks/Pages/IKS.aspx" target="_blank">APL Library</NavLink></NavItem>
+              </Nav>
+            </Navbar>
+          </Col>
+         </Row>
         </Container>
 
-        <Container className="jhu-second-nav col-12">
+        <Container className="jhu-second-nav">
           <Row>
-            <Col className="col-*">
+            <Col className="col-lg-auto col-md-auto col-sm-* col-xs-*">
               <a href="https://catalyst.library.jhu.edu/info/libraries"><img alt="Johns Hopkins University Libraries" id="top-logo" src="/logo2.png" /></a>
             </Col>
-            <Col className="col-*">
-                <a href="/" id="main-title">Databases</a>
+            <Col className="col-lg-auto col-md-auto col-sm-* col-xs-*">
+              <a href="/" id="main-title">Databases</a>
             </Col>
-            <Col className="col-sm-8">
+            <Col className="col-lg-6 col-md-6">
               <Navbar className="jhu-second-nav-links">
                 <Nav>
                   <NavItem><NavLink className="jhu-second-nav" href="/">Browse by Subject</NavLink></NavItem>
