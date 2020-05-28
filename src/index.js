3d13f6b2c5388e824cf689faa36c4d75747c1986
mcyzyk <mcyzyk@jhu.edu>
2020-05-28 16:35:02 -0400

    Corrected Tag Manager; implemented core-js
    This push did two things:
    
    There was an incorrect Google Tag Manager code in public/index.html.  This corrects that
    
    Implemented the core-js polyfill library so that IE will work.  yarn add core-js, then importted it in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import 'core-js';
 import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
