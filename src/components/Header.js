f0a3959f176f4943d82537620e048fcdbf950351
mcyzyk <mcyzyk@jhu.edu>
2020-08-19 16:30:19 -0400

    Update Header.js
    Changes for Friedheim and APL llibraries in header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,8 +13,8 @@ const Header = () => {
                 <NavItem><NavLink className="jhu-first-nav" href="https://library.jhu.edu" target="_blank">Sheridan Libraries</NavLink></NavItem>
                 <NavItem><NavLink className="jhu-first-nav" href="http://welch.jhmi.edu/welchone/" target="_blank">Welch Medical Library</NavLink></NavItem>
                 <NavItem><NavLink className="jhu-first-nav" href="http://www.sais-jhu.edu/atoz/mason-library-washington-dc" target="_blank">SAIS Library</NavLink></NavItem>
-                <NavItem><NavLink className="jhu-first-nav" href="http://musiclibrary.peabody.jhu.edu/home" target="_blank">Friedheim Music Library</NavLink></NavItem>
-                <NavItem><NavLink className="jhu-first-nav" href="https://aplweb/departments/itsd/iks/Pages/IKS.aspx" target="_blank">APL Library</NavLink></NavItem>
+                <NavItem><NavLink className="jhu-first-nav" href="http://musiclibrary.peabody.jhu.edu/home" target="_blank">Arthur Friedheim Library</NavLink></NavItem>
+                <NavItem><NavLink className="jhu-first-nav" href=" https://www.jhuapl.edu/Library/" target="_blank">APL Library</NavLink></NavItem>
               </Nav>
             </Navbar>
           </Col>
