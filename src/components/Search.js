fc88028e5c4c00fbd61d9a8c7431c4b72388663c
Jamie Little <jamie@jamielittle.org>
2025-07-15 07:54:52 -0500

    DBLST-162 Add notice for updated databases list
    This adds a notice to the homepage that a new version of the
    databases list is being launched on August 11th

--- src/components/Search.js
+++ src/components/Search.js
@@ -75,7 +75,8 @@ class Search extends Component {
               </InputGroup>
               <div id="jhutext">Databases provide access to journal articles, newspapers, audio and video recordings, data sets, dissertations, and more.</div>
               <div id="jhutext">Most databases are restricted to students, faculty, and staff of Johns Hopkins and must not be used for commercial or other purposes not related to the mission of Johns Hopkins.</div>
-
+	    <div id="jhutext">The Databases service is getting an updated look on Monday, August 11th. It may be unavailable between 9:00 AM and 12:00 PM Eastern Time. The basic design and function will be the same, and the permanent URL for each database will not change.
+	    </div>
           {
             this.props.searchTerm ? <ResourceList/> : <TagList api={process.env.REACT_APP_API_ROOT + '/oriole/tags'} />
           }
